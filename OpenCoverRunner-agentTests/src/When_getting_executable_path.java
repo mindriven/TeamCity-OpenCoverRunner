5a6a1227e9cb4e6c833395ce2271d3660e12043b
Kamil <3dcreator.pl@gmail.com>
2015-10-03 17:34:23 +0200

    extends test to better match explained error

--- OpenCoverRunner-agentTests/src/When_getting_executable_path.java
+++ OpenCoverRunner-agentTests/src/When_getting_executable_path.java
@@ -44,13 +44,14 @@ public class When_getting_executable_path {
         Assert.assertEquals(userProvidedPath, argument.getValue());
     }
 
-    // inspired by https://github.com/mindriven/TeamCity-OpenCoverRunner/issues/8
     @Test
+    // inspired by https://github.com/mindriven/TeamCity-OpenCoverRunner/issues/8
     public void and_user_specifies_absolute_path__it_gets_returned_as_executable_path() throws Exception {
 
         String userProvidedPath = "C:\\Program Files\\Microsoft Visual Studio 12.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe";
         ConfigValuesProvider configProvider = mock(ConfigValuesProvider.class);
         when(configProvider.getValueOrDefault(OpenCoverRunnerConsts.SETTINGS_OPEN_COVER_PATH)).thenReturn(userProvidedPath);
+        when(configProvider.getValueOrDefault(OpenCoverRunnerConsts.SETTINGS_TEAM_CITY_CHECKOUT_DIR)).thenReturn("X:\\BuildAgent\\work\\9385ca14d240412f");
         ExecutablePathProvider provider = new ExecutablePathProvider(configProvider);
         OpenCoverRunnerDirectoryScanner scanner = new OpenCoverRunnerDirectoryScanner();
         provider.setDirectoryScanner(scanner);
